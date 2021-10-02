e8573e86d6114b2c905d67437785b70cc890b55a
fhctyw <lisnychukarsenw@gmail.com>
2021-10-02 23:44:54 +0300

    add code and block diagram

---
+++ Lab4.4/Lab4.4/Main.cpp
@@ -0,0 +1,49 @@
+// Main.cpp
+// Лісничук Арсен
+// Лабораторна робота № 4.4
+// Табуляція функції, заданої формулою: функція з параметрами
+// Варіант 13
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	double xp, xk, dx, R;
+	double y = 0;
+
+	cout << "R = "; cin >> R;
+	
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+
+	
+	cout << fixed;
+	cout << "----------------------" << endl;
+	cout << "|" << setw(9) << "x |" << setw(12) << "y |" << endl;
+	cout << "----------------------" << endl;
+
+
+	for (double x = xp; x <= xk; x += dx)
+	{
+		if (x <= -R)
+			y = x + R;
+		else
+			if (x > -R && x <= 0)
+				y = sqrt(R * R - x * x);
+			else
+				if (x > 0 && x <= 6)
+					y = R - (R * x) / 6.;
+				else
+					y = x - 6;
+
+		cout << "|" << setw(7) << setprecision(2) << x << " |"
+			<< setw(10) << setprecision(3) << y << " |" << endl;
+	}
+	cout << "----------------------" << endl;
+
+	return 0;
+}
